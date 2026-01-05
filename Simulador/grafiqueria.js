06c253dad67001395106daab7b4447d5b754cc47
pauguz <paulo.guzman1@unmsm.edu.pe>
2026-01-05 05:31:22 -0500

    Correcting some issues

---
+++ Simulador/grafiqueria.js
@@ -0,0 +1,40 @@
+//--VALORES GLOBALES--
+const disBot = document.getElementById("distrito");
+const disSpan= document.getElementById("input-magnitud")
+
+let disUn= false;
+
+function distritoUnico(){
+    disUn = !disUn
+    if(disUn){
+        disBot.innerHTML=
+        `
+        Distrito Multiple
+        `;
+
+        disSpan.innerHTML=      
+        `
+        <label for="magnitud">Magnitud:</label>
+        <input id="magnitud" type="number" placeholder=130>
+        </input>
+        `;
+        return "Distrito Unico Activado"
+    } else{
+        document.getElement
+        disBot.innerHTML=
+        `
+        Distrito Unico
+        `;
+        disSpan.innerHTML= ``;
+        return "Distrito Unico Desactivado"
+    }
+}
+
+disBot.addEventListener('click', () => console.log(distritoUnico()));
+
+/* 
+    <button id="distrito">Distrito Unico</button>
+    <label for="magnitud">Magnitud:</label>
+    <input id="magnitud" type="number" placeholder=130>
+    </input>
+*/
